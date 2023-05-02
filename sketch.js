dde511c18f23ba376549b9f30859695d2ecabb71
Johanalopez0922 <121140087+Johanalopez0922@users.noreply.github.com>
2023-05-02 15:58:15 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,34 +1,13 @@
   
-var towerImg, tower;
-var doorImg, door, doorsGroup;
-var climberImg, climber, climbersGroup;
-var ghost, ghostImg;
-var invisibleBlockGroup, invisibleBlock;
-var gameState = "play"
+
 
 
 function preload(){
-  towerImg = loadImage("tower.png");
-  doorImg = loadImage("door.png");
-  climberImg = loadImage("climber.png");
-  ghostImg = loadImage("ghost-standing.png");
-  spookySound = loadSound("spooky.wav");
+ 
 }
 
 function setup() {
-  createCanvas(600,600);
-  spookySound.loop();
-  tower = createSprite(300,300);
-  tower.addImage("tower",towerImg);
-  tower.velocityY = 1;
   
-  doorsGroup = new Group();
-  climbersGroup = new Group();
-  invisibleBlockGroup = new Group();
-  
-  ghost = createSprite(200,200,50,50);
-  ghost.scale = 0.3;
-  ghost.addImage("ghost", ghostImg);
 }
 
 
@@ -68,30 +47,18 @@ function draw() {
   drawSprites();
 }
   if (gameState === "end"){
-    stroke("yellow");
-    fill("yellow");
-    textSize(30);
-    text("Fin del juego", 230,250)
+   
   }
 }
 
-function spawnDoors()
- {
+function spawnDoors(){
   //escribir aquí el código para aparecer los obstáculos
   if (frameCount % 240 === 0) {
-    var door = createSprite(200, -50);
-    var climber = createSprite(200,10);
-    var invisibleBlock = createSprite(200,15);
-    invisibleBlock.width = climber.width;
-    invisibleBlock.height = 2;
-    //agregar la función random
-    //
-    door.addImage(doorImg);
-    climber.addImage(climberImg);
+   
     
-    door.velocityY = 1;
-    climber.velocityY = 1;
-    invisibleBlock.velocityY = 1;
+    //agregar la función random
+    
+    
 
     //cambiar la profundidad del fantasma y de la puerta
     
